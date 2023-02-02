dbdcbab3427c038a7ecbf13db564041e33ff3d60
Lohit2104 <124122994+Lohit2104@users.noreply.github.com>
2023-02-02 10:12:58 +0530

    Create BFS.py

---
+++ BFS.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+def generateAdjacencyLst(edges):
+    adjacencyList = defaultdict(list)
+    for u, v in edges:
+        adjacencyList[u].append(v)
+        adjacencyList[v].append(u)
+    return adjacencyList
+
+edges = [['A', 'B'], ['A', 'C'], ['B', 'D'], ['B', 'E'], ['C', 'E'], ['C', 'F']]
+adjacencyList = generateAdjacencyLst(edges)
+print(adjacencyList)
+def bfs(adjacencyList, vertex):
+    visitedSet = set()
+    queue = []
+    visitedSet.add(vertex)
+    queue.append(vertex)
+    
+    result = []
+    while queue:
+        v = queue[0]
+        result.append(v)
+        queue = queue[1:]
+        for neighbor in adjacencyList[v]:
+            if neighbor not in visitedSet:
+                visitedSet.add(neighbor)
+                queue.append(neighbor)
+    return result
+
+print(bfs(adjacencyList, 'A'))
