bcfcb305e4b3f0c447dc78f8019d95e328e2a41f
Lohit2104 <124122994+Lohit2104@users.noreply.github.com>
2023-02-04 09:35:02 +0530

    Create student teacher.pl

---
+++ student teacher.pl
@@ -0,0 +1,10 @@
+Studies (charlie, csc135). 
+Studies (olivia, csc135). 
+Studies (jack, csc131). 
+Studies (arthur, csc134).
+Teaches (kirke, csc135). 
+Teaches (collins, csc131). 
+Teaches (collins, csc171). 
+Teaches (juniper, csc134). 
+Professor (X, Y) :- teaches(X, C),
+Studies (Y, C).
